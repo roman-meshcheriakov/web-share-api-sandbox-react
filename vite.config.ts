9832bf78a2b6f47aae572d4f5bff25fe5b205701
Roman Meshcheriakov <roman.meshcheriakov@fundraiseup.com>
2024-10-11 16:56:50 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/web-share-api-sandbox-vue/",
+  base: "/web-share-api-sandbox-react/",
   plugins: [react()],
 });
